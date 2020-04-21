4eb8d121f51d4ad6d502108891d080882b08b7a6
Gayane-pc <sgayaneaslanyan@gmail.com>
2020-04-22 00:15:04 +0400

    create export folder for admin controller

--- app/Exports/AdminExport.php
+++ app/Exports/AdminExport.php
@@ -1,17 +1,30 @@
 <?php
+
 namespace App\Exports;
+
 use App\Models\Admin;
+use App\Repositories\Repository;
 use Maatwebsite\Excel\Concerns\FromCollection;
-use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
 
 class AdminExport implements FromCollection, WithHeadings, ShouldAutoSize
 {
+    protected $model;
+
+    public function __construct()
+    {
+        // set the model
+        $admin = new Admin();
+        $this->model = new Repository($admin);
+    }
+
     public function collection()
     {
-        return Admin::all();
+        return $this->model->all();
     }
+
     public function headings(): array
     {
         return [
