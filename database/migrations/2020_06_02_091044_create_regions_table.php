e297832431359cc9d2100179583fc8f6e1ba5284
Gayane-pc <sgayaneaslanyan@gmail.com>
2020-06-09 14:25:57 +0400

    09.06.2020

---
+++ database/migrations/2020_06_02_091044_create_regions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('regions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',255);
+            $table->bigInteger('region_id')->unsigned()->nullable();
+            $table->enum('status',['region','city','territory','village']);
+            $table->timestamp('updated_at')->useCurrent();
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('regions');
+    }
+}
