231bca4a296374b81d27a12326eeec183db5b11e
Gayane-pc <sgayaneaslanyan@gmail.com>
2020-10-18 21:46:06 +0400

    Merge branch 'master' of https://github.com/gaslanyan/training
    # Conflicts:
    #	app/Http/Controllers/Backend/CoursesController.php
    #	database/migrations/2020_08_20_174605_2020_05_07_224021_add_foreign_keys_to_courses_table.php
    #	database/migrations/2020_08_20_174605_add_foreign_keys_to_courses_table.php
    #	database/migrations/2020_09_01_130610_add_foreign_keys_to_accounts_videos_table.php
    #	resources/sass/_custom.scss

---
+++ resources/js/components/Books.vue
@@ -0,0 +1,22 @@
+<!--
+<template>
+
+    <VuePdfJs class="grey"
+              :url="url"
+    />
+
+</template>
+
+<script>
+    import VuePdfJs from "vue-pdfjs-demo/src/App.vue";
+
+    export default {
+        components: {
+            VuePdfJs
+        },
+        data: () => ({
+            url:'https://cors-anywhere.herokuapp.com/YourUrl.pdf'
+        })
+    }
+</script>
+-->
