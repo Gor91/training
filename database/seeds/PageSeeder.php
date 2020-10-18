2074e9b42b4bcbd795d90730e86ccde2a4b7e116
Gayane-pc <sgayaneaslanyan@gmail.com>
2020-10-18 22:26:10 +0400

    Merge branch 'master' of https://github.com/gaslanyan/training
    # Conflicts:
    #	app/Http/Controllers/Backend/CoursesController.php
    #	database/migrations/2020_08_20_174605_2020_05_07_224021_add_foreign_keys_to_courses_table.php
    #	database/migrations/2020_08_20_174605_add_foreign_keys_to_courses_table.php
    #	database/migrations/2020_09_01_130610_add_foreign_keys_to_accounts_videos_table.php
    #	resources/sass/_custom.scss

--- database/seeds/PageSeeder.php
+++ database/seeds/PageSeeder.php
@@ -27,6 +27,7 @@ class PageSeeder extends Seeder
                 "title" => "Մեր Մասին",
                 "slug" => "about",
                 "description" => "There is a moment in the life of any aspiring astronomer that it is time to buy that first telescope. It’s exciting to think about setting up your own viewing station. In the life of any aspiring astronomer that it is time to buy that first telescope. It’s exciting to think about setting up your own viewing station.\r\n\r\nIt’s exciting to think about setting up your own viewing station. In the life of any aspiring astronomer that it is time to buy that first telescope exciting is time to buy that first.",
+                "homedescription" => "There is a moment in the life of any aspiring astronomer that it is time to buy that first telescope. It’s exciting to think about setting up your own viewing station. In the life of any aspiring astronomer that it is time to buy that first telescope. It’s exciting to think about setting up your own viewing station.\r\n\r\nIt’s exciting to think about setting up your own viewing station. In the life of any aspiring astronomer that it is time to buy that first telescope exciting is time to buy that first.",
                 "created_at" => Carbon::now(),
                 "updated_at" => Carbon::now()
             ]
